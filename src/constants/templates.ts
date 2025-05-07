900ad1b4b1819b0bb0b92c07cd96be9f2aa112c2
ALOK0078 <alokkumar0709003@gmail.com>
2025-05-08 00:49:39 +0530

    home screen setup

---
+++ src/constants/templates.ts
@@ -0,0 +1,41 @@
+export const templates =[
+    {
+        id:"blank",
+        label:"Blank Document",
+        imageUrl:"/blank-document.svg"
+    },
+    {
+        id:"software-proposal",
+        label:"Software Proposal",
+        imageUrl:"/software-proposal.svg"
+    },
+    {
+        id:"project-proposal",
+        label:"Project Proposal",
+        imageUrl:"/project-proposal.svg"
+    },
+    {
+        id:"bussiness-letter",
+        label:"Bussiness Letter",
+        imageUrl:"/bussiness-letter(1).svg"
+    },
+    {
+        id:"resume",
+        label:"Resume",
+        imageUrl:"/resume.svg"
+    },
+    {
+        id:"cover-letter",
+        label:"Cover Letter",
+        imageUrl:"/cover-letter.svg"
+    },
+    {
+        id:"letter",
+        label:"Normal Letter",
+        imageUrl:"/letter.svg"
+    }
+
+
+
+
+]
